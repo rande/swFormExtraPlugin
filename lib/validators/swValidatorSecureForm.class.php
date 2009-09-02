63a2984d2de67288b1ac747f5275c40c7ae9446a
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-09-02 15:10:34 +0000

    [swFormExtraPlugin] fix typo
    git-svn-id: http://svn.symfony-project.com/plugins/swFormExtraPlugin/trunk@21646 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validators/swValidatorSecureForm.class.php
+++ lib/validators/swValidatorSecureForm.class.php
@@ -1,13 +1,17 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * This file is part of the swFormExtraPlugin package.
+ *
+ * (c) 2008 Thomas Rabaix <thomas.rabaix@soleoweb.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 /**
- * Description of swValidatorSecureFormclass
  *
- * @author thomas
+ * @author Thomas Rabaix <thomas.rabaix@gmail.com>
+ * 
  */
 class swValidatorSecureForm extends sfValidatorBase
 {
